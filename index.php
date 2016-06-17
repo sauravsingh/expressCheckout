a80df2ab6ff407468f1d968a9cb557dadae0f06c
Saurav <sauravsingh@users.noreply.github.com>
2016-06-17 17:28:30 +0530

    create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<h2>Express Checkout</h2>
+	<form action='expresscheckout.php' METHOD='POST'>
+		<table>
+        	<tbody>
+		<tr>
+			<td>Amount (in $)</td>
+			<td>
+				<input type="text" name="Payment_Amount" placeholder="Enter Amount">
+			</td>
+		</tr>
+            </tbody>
+        </table>
+		<input type='submit' name='submit' value='Buy' />
+	</form>
+</body>
+</html>
